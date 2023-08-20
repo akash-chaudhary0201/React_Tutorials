4986d3d892470aad1d8b5a6088a0b9b5d5663f2b
Akash Chaudhary <128380952+akash-chaudhary0201@users.noreply.github.com>
2023-08-20 11:51:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import "./App.css"
+import React, {} from "react";
+// import Basic_form_validation from "./components/Basic_form_validation";
+// import Input from "./components/Input";
+// import Hide_Show_Toggle from "./components/Hide_Show_Toggle";
+// import Handle_form from "./components/Handle_form"m
+// import If_Else from "./components/If_Else";
+import {} from 'react-bootstrap'
+
+
+function App() {
+
+  return (
+    <>
+      {/* <Input/> */}
+      {/* <Hide_Show_Toggle/> */}
+      {/* <Handle_form/> */}
+      {/* <If_Else/> */}
+      {/* <Basic_form_validation/> */}
+      
+    </>
+  );
+}
+
+export default App;
